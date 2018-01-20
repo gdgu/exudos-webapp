4b600a123d4ef2ed0675013c629fa7c8a7a0a7df
SwG Ghosh <codecrafts.cf@icloud.com>
2018-01-20 19:55:09 +0530

    Small changes to 404 content

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ var querystring = require('querystring');
 var htmldyn = require('./htmldynmodule.js');
 
 // create the server
-var server = http.createServer((req, res) => { 
+var server = http.createServer((req, res) => {
     if(req.url == '/') {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        
+
         fs.readFile('htdocs/index.html', 'utf8', (err, data) => {
             if(err) throw err;
 
@@ -21,7 +21,7 @@ var server = http.createServer((req, res) => {
     }
     else if(req.url == '/base.css') {
         res.writeHead(200, {'Content-Type': 'text/css'});
-        
+
         fs.readFile('htdocs/base.css', 'utf8', (err, data) => {
             if(err) throw err;
 
@@ -30,16 +30,42 @@ var server = http.createServer((req, res) => {
     }
     else {
         res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end((() => 
-            htmldyn.getHtmlTagString('html', '\n' + htmldyn.getHtmlTagString('head', [htmldyn.getHtmlTagString('meta', undefined, undefined, undefined, {
-                name: 'viewport',
-                content: 'width=device-width'
-            }), htmldyn.getHtmlTagString('meta', undefined, undefined, undefined, {
-                charset: 'utf8'
-            }), htmldyn.getHtmlTagString('title', '404 | Error Page')].join('\n')) + '\n' + htmldyn.getHtmlTagString('body', '\n' + [htmldyn.getHtmlTagString('h1', '404 Error'), htmldyn.getHtmlTagString('p', 'This is not the page that you\'re looking for')].join('\n') + '\n') + '\n', undefined, undefined, {
+        res.end('<!DOCTYPE html>' +
+            htmldyn.getHtmlTagString('html', '\n' +
+                htmldyn.getHtmlTagString('head',
+                    [
+                        htmldyn.getHtmlTagString('meta', undefined, undefined, undefined, {
+                            name: 'viewport',
+                            content: 'width=device-width'
+                        }),
+                        htmldyn.getHtmlTagString('meta', undefined, undefined, undefined, {
+                            charset: 'utf8'
+                        }),
+                        htmldyn.getHtmlTagString('link', undefined, undefined, undefined, {
+                            href: 'base.css',
+                            rel: 'stylesheet',
+                            type: 'text/css'
+                        }),
+                        htmldyn.getHtmlTagString('title', '404 | Error Page')
+                    ].join('\n')
+                ) +
+                '\n' +
+                htmldyn.getHtmlTagString('body', '\n' +
+                    [
+                        htmldyn.getHtmlTagString('h1', '404. Page not found. :()'),
+                        htmldyn.getHtmlTagString('p',
+                            [
+                                'Oops! That\'s an error.',
+                                htmldyn.getHtmlTagString('br'),
+                                'This is not the page that you\'re looking for.'
+                            ].join('\n')
+                        )
+                    ].join('\n')) +
+                '\n',
+            undefined, undefined, {
                 lang: 'en'
             })
-        )());
+        );
     }
 });
 
