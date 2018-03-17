4d455bc3afe2263040b09b8f9f3d35ec78d64ed0
SwG Ghosh <codecrafts.cf@icloud.com>
2018-03-18 03:42:58 +0530

    Removed an unnecessary file and added comments in dynservermodule.js

--- dynservermodule.js
+++ dynservermodule.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// import required modules
 var url = require('url');
 var fs = require('fs');
 
@@ -7,26 +8,36 @@ var htmldyn = require('./htmldynmodule.js');
 
 class DynamicServer {
     constructor(webroot, dynPages) {
+        // type errors
         if(typeof webroot !== 'string') throw new TypeError('first argument "webroot" must be of type string, ' + typeof webroot + ' found');
         if(typeof dynPages !== 'object') throw new TypeError('second argument "dynPages" must be of type object, ' + typeof dynPages + ' found');
+        // list of dynamically serviceable pages and the path to webroot directory
         this.dynPages = dynPages;
         this.webroot = webroot;
     }
     serve(req, res) {
         var parsedUrl = url.parse(req.url);
-        // in case of file alias
+        // in case of alias
         if(this.dynPages[parsedUrl.pathname].alias !== undefined) {
+            // original file path that has been aliased
             parsedUrl.pathname = this.dynPages[parsedUrl.pathname].alias;
+            // modify url path
             req.url = parsedUrl.format();
+            // recurse to serve the aliased file
             this.serve(req, res);
         }
-        // serve a file
+        // in case of not an alias, serve a file
         else {
+            // path to the file containing dynamic values
             var dynFilePath = this.dynPages[parsedUrl.pathname].dynvalues;
+            // fetch dynamic values
             var values = require(__dirname + '/' + dynFilePath).values;
+            // read html content of file
             fs.readFile(this.webroot + parsedUrl.pathname, this.dynPages[parsedUrl.pathname].encoding, (err, data) => {
+                // in case of error
                 if(err) throw new Error("Could not read file" + webroot + parsedUrl.pathname + ".\n" + err.message);
 
+                // pass the response by altering html data of file with the dynamic values using htmldyn
                 res.writeHead(200, {"Content-Type": this.dynPages[parsedUrl.pathname].type});
                 res.end(htmldyn.getHtmlStringWithIdValues(data, values), this.dynPages[parsedUrl.pathname].encoding);
             });
@@ -34,4 +45,5 @@ class DynamicServer {
     }
 }
 
+// expose the class
 exports.Server = DynamicServer;
--- unittest.js
+++
@@ -1,5 +0,0 @@
-var assert = require('assert');
-
-const webroot = __dirname + '/htdocs';
-const htdocs = JSON.parse(fs.readFileSync('htdocs.json', 'utf8'));
-
