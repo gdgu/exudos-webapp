ff7df45cb31831adcac5e72620c5c4f1838a4c09
SwG Ghosh <codecrafts.cf@icloud.com>
2018-04-10 01:43:05 +0530

    Added code to login.dyn.js to auth based on test cookie already set

--- dyns/login.dyn.js
+++ dyns/login.dyn.js
@@ -1,9 +1,29 @@
 var fs = require('fs');
+var crypto = require('crypto');
+
+// hashing type is RSA
+const hashType = 'RSA-SHA';
 
 exports.makeValues = (req, res, cookies, data) => {
     var values = JSON.parse(fs.readFileSync('dyns/globalvars.json', 'utf8'));
-    values.username = 'test.student';
-    values.userFullName = 'John Smith';
+
+    if(cookies['lTokenA'] !== undefined && cookies['lTokenB'] !== undefined) {
+        
+        var passwordHash = crypto.createHash(hashType);
+        passwordHash.update('abcd123');
+
+        var userPassword = passwordHash.digest('hex');
+
+        if(cookies['lTokenA'] === 'test.student' && cookies['lTokenB'] === userPassword) {
+            values.username = 'test.student';
+            values.userFullName = 'John Smith';
+        }
+    }
+
+    else {       
+        values.username = 'NOUSER';
+        values.userFullName = 'NO NAME AS NO LOGIN';
+    }
 
     return values;
 }
