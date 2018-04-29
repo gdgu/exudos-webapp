2abf8ad5137079b176e2b3b6047fd67efe4aec94
SwG Ghosh <codecrafts.cf@icloud.com>
2018-04-30 02:14:38 +0530

    Added all submenu items in landing.html without the interactivity

---
+++ htdocs/submenu.js
@@ -0,0 +1,37 @@
+var listOfMenuItems = [
+    {
+        id: 'assignments',
+        title: 'Assignments',
+        submenu: [
+            'createassignment', 'submitassignment', 'listassignment'
+        ]
+    },
+    {
+        id: 'forums',
+        title: 'Forum',
+        submenu: [
+            'newchat', 'messages', 'notices', 'uploadnotice'
+        ]
+    },
+    {
+        id: 'evaluation',
+        title: 'Evaluation',
+        submenu: [
+            'internalmarks', 'marksreport'
+        ]
+    },
+    {
+        id: 'courses',
+        title: 'Courses',
+        submenu: [
+            'uploadcoursematerial', 'mycourses', 'mytimetable'
+        ]
+    },
+    {
+        id: 'feedback',
+        title: 'Feedback',
+        submenu: [
+            'assignmentfeedback', 'facultyfeedback'
+        ]
+    }
+]
