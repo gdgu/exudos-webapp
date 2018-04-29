095b6caf5997c347849e073ec768f011aab43039
SwG Ghosh <codecrafts.cf@icloud.com>
2018-04-30 02:50:20 +0530

    Added hover based interactivity in landing.html submenus

--- htdocs/submenu.js
+++ htdocs/submenu.js
@@ -4,34 +4,70 @@ var listOfMenuItems = [
         title: 'Assignments',
         submenu: [
             'createassignment', 'submitassignment', 'listassignment'
-        ]
+        ],
+        togglestate: false
     },
     {
         id: 'forums',
         title: 'Forum',
         submenu: [
             'newchat', 'messages', 'notices', 'uploadnotice'
-        ]
+        ],
+        togglestate: false
     },
     {
         id: 'evaluation',
         title: 'Evaluation',
         submenu: [
             'internalmarks', 'marksreport'
-        ]
+        ],
+        togglestate: false
     },
     {
         id: 'courses',
         title: 'Courses',
         submenu: [
             'uploadcoursematerial', 'mycourses', 'mytimetable'
-        ]
+        ],
+        togglestate: false
     },
     {
         id: 'feedback',
         title: 'Feedback',
         submenu: [
             'assignmentfeedback', 'facultyfeedback'
-        ]
+        ],
+        togglestate: false
     }
 ]
+
+function hideAllSubmenus() {
+    for(let index = 0; index < listOfMenuItems.length; index++) {
+        var menuId = listOfMenuItems[index].id;
+        let expandedSubmenus = document.querySelector('div.expanded#' + menuId);
+        if(listOfMenuItems[index].togglestate) {
+            expandedSubmenus.style.visibility = 'hidden';
+            listOfMenuItems[index].togglestate = false;
+        }
+    }
+}
+
+for(let index = 0; index < listOfMenuItems.length; index++) {
+    var menuId = listOfMenuItems[index].id;
+    let expandedSubmenus = document.querySelector('div.expanded#' + menuId);
+    // set their visibility to hidden by default
+    expandedSubmenus.style.visibility = 'hidden';
+
+    var navTabItem = document.querySelector('div.menubar ul.tabs li#' + menuId);
+    navTabItem.addEventListener('mouseover', function() {
+        if(listOfMenuItems[index].togglestate) {
+            hideAllSubmenus()
+        }
+        else {
+            hideAllSubmenus()
+            expandedSubmenus.style.visibility = '';
+
+            listOfMenuItems[index].togglestate = !listOfMenuItems[index].togglestate;
+        }
+    });
+}
