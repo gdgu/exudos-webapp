a2197e223749fab356038f14567f63fa37f87654
SwG Ghosh <codecrafts.cf@icloud.com>
2018-05-12 03:36:52 +0530

    Prevent from accessing student -> faculty and faculty -> student in respective index

--- dyns/faculty/index.dyn.js
+++ dyns/faculty/index.dyn.js
@@ -7,7 +7,17 @@ var auth = require('../auth.dyn')
 var htmldynmodule = require('../../lib/htmldyn/htmldynmodule')
 
 exports.servePage = (req, res, options) => {
-    auth.postAuth(req, res, (currentUser, userType) => {
+    auth.postAuth(req, res, (currentUser, currentUserType) => {
+
+        if(userType !== currentUserType) {
+
+            res.writeHead(302, {
+                'Location': '/landing.do'
+            })
+            res.end()
+            
+            return
+        }
 
         var values = JSON.parse(fs.readFileSync('dyns/globalvars.json', 'utf8'));
 
