e96bf84c2934f0168aec42bae669beee6d3918e4
SwG Ghosh <codecrafts.cf@icloud.com>
2018-05-12 03:30:24 +0530

    Added submenu in student,faculty/index.html appropriate auth added

---
+++ dyns/student/index.dyn.js
@@ -0,0 +1,28 @@
+const userType = 'student'
+
+var fs = require('fs')
+
+var auth = require('../auth.dyn')
+
+var htmldynmodule = require('../../lib/htmldyn/htmldynmodule')
+
+exports.servePage = (req, res, options) => {
+    auth.postAuth(req, res, (currentUser, userType) => {
+
+        var values = JSON.parse(fs.readFileSync('dyns/globalvars.json', 'utf8'));
+
+        values.username = currentUser.username
+        values.usertype = userType
+
+        res.writeHead(200, {
+            'Content-Type': options.type
+        })
+
+        fs.readFile(options.filepath, options.encoding, (err, data) => {
+            res.end(
+                htmldynmodule.getHtmlStringWithIdValues(data, values)
+            )
+        })
+
+    })
+}
