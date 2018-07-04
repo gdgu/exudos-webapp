a2b5e30ed7a872e643f950c86e35747e7c58dc4c
SwG Ghosh <codecrafts.cf@icloud.com>
2018-07-05 01:25:41 +0530

    Minor changes

--- dyns/upload.dyn.js
+++ dyns/upload.dyn.js
@@ -1,3 +1,5 @@
+var mimeTypes = require('mime-types')
+
 var auth = require('./auth')
 var bodyparsermodule = require('../lib/htmldyn/bodyparsermodule');
 
@@ -17,6 +19,7 @@ exports.servePage = (req, res, body) => {
         var matches = blob.match(/data:(.+\/.+);base64,(.*)/)
 
         var document = new Document(Buffer.from(matches[2], 'base64'), matches[1])
+        var extension = mimeTypes.extension(document.mimeType)
 
         blDocuments.addDocument(document, (flag, idString) => {
             res.writeHead(200, {
@@ -24,7 +27,7 @@ exports.servePage = (req, res, body) => {
             })
 
             if(flag) {
-                res.end(idString)
+                res.end('_' + idString + '.' + extension)
             }
             else {
                 res.end('failed upload', idString)
