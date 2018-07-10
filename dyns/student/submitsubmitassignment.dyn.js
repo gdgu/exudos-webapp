35216916f5528146028d5a1ce4104a7f31ac7d46
SwG Ghosh <codecrafts.cf@icloud.com>
2018-07-10 23:25:00 +0530

    Added code to allow submitassignment in student (multiple additional fixes including fix dbmanip for null queryid)

--- dyns/student/submitsubmitassignment.dyn.js
+++ dyns/student/submitsubmitassignment.dyn.js
@@ -1,11 +1,19 @@
 const userType = 'student'
 
 var fs = require('fs')
+var events = require('events')
 
 var auth = require('../auth')
 var wrongUserType = require('../wrongusertype')
 
 var bodyparsermodule = require('../../lib/htmldyn/bodyparsermodule')
+
+var blAssignments = require('../../lib/bl/assignments')
+var blIdNames = require('../../lib/bl/idnames')
+var blDocuments = require('../../lib/bl/documents')
+
+var AssignmentSubmission = require('../../lib/models/AssignmentSubmission')
+var Document = require('../../lib/models/Document')
 
 exports.filePath = ''
 
@@ -22,10 +30,64 @@ exports.servePage = (req, res, body) => {
 
         var postParams = bodyparsermodule.parseHttpBody(body);
 
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
+        var page = new events.EventEmitter()
+        page.on('failed', () => {
+            res.writeHead(302, {
+                'Content-Type': 'text/plain',
+                'Location': 'submitassignment.html?failed'
+            })
+            res.end('form submission failed, redirection issued')
         })
-        res.end(JSON.stringify(postParams))
+        page.on('success', () => {
+            res.writeHead(302, {
+                'Content-Type': 'text/plain',
+                'Location': 'submitassignment.html?success'
+            })
+            res.end('form submission successful, redirection issued')
+        })
 
+        var dataUrlRegex = /data:(.+\/.+);base64,(.*)/
+        
+        if(postParams['id'] == null || postParams['id'] == '' || postParams['blob'] == null || postParams['blob'] == null) {
+            page.emit('failed')
+        }
+
+        else if(postParams['blob'].match(dataUrlRegex) == null) {
+            page.emit('failed')
+        }
+
+        else {
+
+            var assignmentIdName = postParams['id']
+            var documentDataMatches = postParams['blob'].match(dataUrlRegex)
+
+            blIdNames.getObjectId(assignmentIdName, (assignmentObjectId) => {
+
+                if(assignmentObjectId == null) {
+                    page.emit('failed')
+                }
+
+                else {
+                    var document = new Document(Buffer.from(documentDataMatches[2], 'base64'), documentDataMatches[1])
+
+                    blDocuments.addDocument(document, (flag, documentId) => {
+                        if(!flag) {
+                            page.emit('failed')
+                        }
+                        else {
+                            var assignmentSubmission = new AssignmentSubmission(assignmentObjectId, documentId, new Date(), currentUser[userType])
+                            blAssignments.submitAssignment(assignmentSubmission, (flag) => {
+                                if(!flag) {
+                                    page.emit('failed')
+                                }
+                                else {
+                                    page.emit('success')
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        }
     })
 }
