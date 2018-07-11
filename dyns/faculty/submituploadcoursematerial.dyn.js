a4ecc66b8e8f40051ae12df3d8fe1eade81fea51
SwG Ghosh <codecrafts.cf@icloud.com>
2018-07-11 14:11:31 +0530

    Added code for submituploadcoursematerial that can fully handle the form

--- dyns/faculty/submituploadcoursematerial.dyn.js
+++ dyns/faculty/submituploadcoursematerial.dyn.js
@@ -1,11 +1,18 @@
 const userType = 'faculty'
 
 var fs = require('fs')
+var events = require('events')
 
 var auth = require('../auth')
 var wrongUserType = require('../wrongusertype')
 
 var bodyparsermodule = require('../../lib/htmldyn/bodyparsermodule')
+
+var blCourses = require('../../lib/bl/courses')
+var blDocuments = require('../../lib/bl/documents')
+
+var CourseMaterial = require('../../lib/models/CourseMaterial')
+var Document = require('../../lib/models/Document')
 
 exports.filePath = ''
 
@@ -21,11 +28,65 @@ exports.servePage = (req, res, body) => {
         }
 
         var postParams = bodyparsermodule.parseHttpBody(body);
-
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
+        var page = new events.EventEmitter()
+        page.on('failed', () => {
+            res.writeHead(302, {
+                'Content-Type': 'text/plain',
+                'Location': 'uploadcoursematerial.html?failed'
+            })
+            res.end('form submission failed, redirection issued')
         })
-        res.end(JSON.stringify(postParams))
+        page.on('success', () => {
+            res.writeHead(302, {
+                'Content-Type': 'text/plain',
+                'Location': 'uploadcoursematerial.html?success'
+            })
+            res.end('form submission successful, redirection issued')
+        })
+
+        var dataUrlRegex = /data:(.+\/.+);base64,(.*)/
+        
+        if(postParams['code'] == null || postParams['code'] == '' || postParams['blob'] == null || postParams['blob'] == '') {
+            page.emit('failed')
+        }
+
+        else if(postParams['blob'].match(dataUrlRegex) == null) {
+            page.emit('failed')
+        }
+
+        else {
+
+            var courseCode = postParams['code']
+            var documentDataMatches = postParams['blob'].match(dataUrlRegex)
+
+            blCourses.getCourseObjectId({code: courseCode}, (courseObjectId) => {
+
+                if(courseObjectId == null) {
+                    page.emit('failed')
+                }
+
+                else {
+
+                    var document = new Document(Buffer.from(documentDataMatches[2], 'base64'), documentDataMatches[1])
+                    blDocuments.addDocument(document, (flag, documentId) => {
+                        if(!flag) {
+                            page.emit('failed')
+                        }
+                        else {
+                            var courseMaterial = new CourseMaterial(documentId, new Date(), courseObjectId, currentUser[userType])
+                            blCourses.createCourseMaterial(courseMaterial, (flag) => {
+                                if(!flag) {
+                                    page.emit('failed')
+                                }
+                                else {
+                                    page.emit('success')
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        }
 
     })
 }
